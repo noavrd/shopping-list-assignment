675587f1c31b7777281c56468d7036c41270a298
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-24 14:36:16 +0200

    finish step 6

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ let button = document.querySelector("button");
 button.onclick = function() {
  let inputValue = input.value;
  input.value = "";   
+
+ let listItem = document.createElement("li");
+ let listText = document.createElement("span");
+ let secButton = document.createElement("button");
+
 }
